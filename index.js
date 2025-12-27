5e2690189137b717d27dd687ec303722642eb85c
Roman Khodosov <hodosov_roman@alexschool.ukr.education>
2025-12-27 10:07:36 +0200

    доробив помилки

--- index.js
+++ index.js
@@ -1,32 +1,41 @@
+//1
 let celsius = 6;
 let fahrenheit;
 fahrenheit = (celsius * 9) / 5 + 32;
 console.log(fahrenheit);
+//2
 let daysInMonth = 30;
 let hoursInMonth = daysInMonth * 24;
 console.log(hoursInMonth);
 let minutesInMonth = hoursInMonth * 60;
 console.log(minutesInMonth);
+//3
 let playerHealth = 100;
 let playerStamina = 100;
-console.log(playerHealth - 70);
-console.log(playerStamina - 59);
-let totalPrice = 597;
-let discount = 0.1;
-let discountedPrice = totalPrice - totalPrice * discount;
+console.log((playerHealth = playerHealth - 70));
+console.log((playerStamina = playerStamina - 41));
+//4
+const totalPrice = 597;
+const discount = 0.1;
+const discountedPrice = totalPrice - totalPrice * discount;
 console.log(discountedPrice);
+//5
 const floatNumber = 12.78;
 const roundedDown = Math.floor(floatNumber);
 console.log(roundedDown);
+//6
 const floatString = "45.67";
 const parsedFloat = parseFloat(floatString);
 console.log(parsedFloat);
+//7
 const intString = "123";
 const parsedInt = parseInt(intString);
 console.log(parsedInt);
-let number = 16;
-let sqrtNumber = Math.sqrt(number);
+//8
+const number = 16;
+const sqrtNumber = Math.sqrt(number);
 console.log(sqrtNumber);
+//9
 const integer = 42;
 const stringNumber = "256";
 const convertedInt = parseInt(stringNumber);
